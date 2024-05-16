c7360951d935f63151f71170dd0edcdd19372511
nitroEef <mikebodetelli@gmail.com>
2024-05-16 16:35:36 +0100

    update

--- jwtAuth8/middleware/verifyJWT.js
+++ jwtAuth8/middleware/verifyJWT.js
@@ -1,12 +1,11 @@
-
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const verifyJWT = async (req, res, next) => {
-    const authHeader = req.headers["authorization"]
+    const authHeader = req.headers.authorization || req.headers.Authorization
 
-    if (!authHeader) return res.sendStatus (401);
-    console.log(authHeader); // Bearer
+    if (!authHeader.startsWith('Bearer')) return res.sendStatus (401);
+    console.log(authHeader); // Bearer token
 
     const token = authHeader.split(" ")[1]
 
@@ -15,7 +14,8 @@ const verifyJWT = async (req, res, next) => {
         process.env.ACCESS_TOKEN_SECRET,
         (err, decoded) => {
             if(err) return res.sendStatus(403); // Invalid token
-            req.user = decoded.username
+            req.user = decoded.UserInfo.username;
+            req.user = decoded.UserInfo.roles;
             next()
         }
     )
--- jwtAuth8/routes/api/employee.js
+++ jwtAuth8/routes/api/employee.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router(); 
 const employeesController = require("../../controllers/employeesController")
-// const verifyJWT = require("../../middleware/verifyJWT")
-
-
+const ROLES_LIST = require("../../config/roles_list");
+const verifyRoles = require("../../config/verifyRoles");
 
 router
     .route("/")
     .get(employeesController.getAllEmployees)
-    .get(employeesController.getAllEmployees)
-    .post(employeesController.createNewEmployee)
-    .put(employeesController.updateEmployee)
-    .delete(employeesController.deleteEmployee)
+    .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),employeesController.createNewEmployee)
+    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),employeesController.createNewEmployee)
+    .delete(verifyRoles(ROLES_LIST.Admin), employeesController.deleteEmployee)
+
+
 
     router.route("/:id").get(employeesController.getEmployee)
 
