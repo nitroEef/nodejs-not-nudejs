42e7dc5064dc4fc7089c3e795db53577a1e1baeb
nitroEef <mikebodetelli@gmail.com>
2024-05-22 16:02:50 +0100

    update

--- MongoTut/controllers/employeesController.js
+++ MongoTut/controllers/employeesController.js
@@ -60,7 +60,8 @@ const deleteEmployee = async (req, res) => {
 const getEmployee = async (req, res) => {
   if (!req?.params?.id) {
     return res.status(400).json({ message: "ID is required." });
-    const employee = await Employee.findOne({ _id: req.body.id }).exec();
+
+    const employee = await Employee.findOne({ _id: req.params.id }).exec();
 
     if (!employee) {
       return res
